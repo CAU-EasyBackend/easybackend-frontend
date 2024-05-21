ca907fce253347e14ee778cbe22a8d8b35cb1f6f
kim yeon <kimkite111@gmail.com>
2024-05-21 20:43:08 +0900

    코드생성페이지

--- lib/api/apicode.dart
+++ lib/api/apicode.dart
@@ -1,15 +1,13 @@
-import 'dart:typed_data'; // Import the dart:typed_data library for Uint8List
-import 'package:easyback/baepopage/sangtae.dart';
-import 'package:flutter/material.dart';
-import 'package:file_picker/file_picker.dart'; // file_picker 라이브러리 추가
-import '../mainpage/ApiPage.dart';
-import '../loginpage.dart';
-import '../mainpage/Deployment.dart';
-import 'package:http/http.dart' as http; // http 패키지 추가
 import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:http/http.dart' as http;
 
-import 'apidetails.dart'; // JSON 디코딩을 위한 패키지 추가
-
+import '../loginpage.dart';
+import '../mainpage/ApiPage.dart';
+import '../mainpage/Deployment.dart';
+import 'apidetails.dart';
 
 class apicode extends StatefulWidget {
   const apicode({Key? key});
@@ -19,20 +17,39 @@ class apicode extends StatefulWidget {
 }
 
 class _apicodeState extends State<apicode> {
-  Color buttonColor1 = Colors.black; // 기본 색상을 검은색으로 설정
+  Color buttonColor1 = Colors.black;
   Color buttonColor2 = Colors.black;
   Color buttonColor3 = Colors.white12;
+  String? centerText;
+  String showFileName = "";
+  String userInput = "";
+  Color defaultColor = Colors.grey[400]!;
+
+  bool checkBox1 = false;
+  bool checkBox2 = false;
+  bool checkBox3 = false;
+  bool checkBox4 = false;
+  bool fileUploadEnabled = true;
+  bool textFieldEnabled = true;
+  String fileContent = "";
 
   @override
   void initState() {
     super.initState();
-    setState(() {
-      buttonColor1 = Colors.black; // 배포 가이드 버튼 색상 변경
-      buttonColor2 = Colors.black; // 파일 업로드 버튼 색상 변경
-      buttonColor3 = Colors.white12; // 상태관리 버튼 색상 변경
-    });
+    setState(() {});
   }
 
+  void _handleCheckboxChanged(bool? value, int group, int index) {
+    setState(() {
+      if (group == 1) {
+        checkBox1 = index == 1 ? value! : false;
+        checkBox2 = index == 2 ? value! : false;
+      } else if (group == 2) {
+        checkBox3 = index == 3 ? value! : false;
+        checkBox4 = index == 4 ? value! : false;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -98,35 +115,36 @@ class _apicodeState extends State<apicode> {
         ),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(height: 50),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 가로로 동일한 간격으로 배치
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
-                width: 200, // 첫 번째 컨테이너의 너비를 설정
+                width: 200,
                 height: 500,
                 decoration: BoxDecoration(
-                  color: Colors.black, // 배경색을 검정으로 설정
-                  borderRadius: BorderRadius.circular(10), // 테두리의 모서리를 둥글게 설정
-                  border: Border.all(color: Colors.white, width: 2), // 테두리를 흰색으로 설정
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.white, width: 2),
                 ),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     SizedBox(height: 2),
                     SizedBox(
-                      width: double.infinity, // 버튼의 너비를 컨테이너의 너비와 동일하게 설정
+                      width: double.infinity,
                       height: 50,
                       child: ElevatedButton(
                         onPressed: () {
-                          _handleapipageButton(context);  // 배포 가이드 버튼 클릭 시 이동하는 함수 호출
+                          _handleapipageButton(context);
                         },
                         style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white, backgroundColor: buttonColor1,
+                          foregroundColor: Colors.white,
+                          backgroundColor: buttonColor1,
                           shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.zero, // 직각 테두리로 설정
+                            borderRadius: BorderRadius.zero,
                           ),
                         ),
                         child: Text('API 가이드'),
@@ -134,14 +152,15 @@ class _apicodeState extends State<apicode> {
                     ),
                     SizedBox(height: 0),
                     SizedBox(
-                      width: double.infinity, // 버튼의 너비를 컨테이너의 너비와 동일하게 설정
+                      width: double.infinity,
                       height: 50,
                       child: ElevatedButton(
                         onPressed: () {
-                          _handleapiguideButton(context); // 배포 가이드 버튼 클릭 시 이동하는 함수 호출
+                          _handleapiguideButton(context);
                         },
                         style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white, backgroundColor: buttonColor2,
+                          foregroundColor: Colors.white,
+                          backgroundColor: buttonColor2,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.zero,
                             side: BorderSide(color: Colors.white),
@@ -152,17 +171,18 @@ class _apicodeState extends State<apicode> {
                     ),
                     SizedBox(height: 0),
                     SizedBox(
-                      width: double.infinity, // 버튼의 너비를 컨테이너의 너비와 동일하게 설정
+                      width: double.infinity,
                       height: 50,
                       child: ElevatedButton(
                         onPressed: () {
-                          _handleapicodeButton(context); // 배포 가이드 버튼 클릭 시 이동하는 함수 호출
+                          _handleapicodeButton(context);
                         },
                         style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white, backgroundColor: buttonColor3,
+                          foregroundColor: Colors.white,
+                          backgroundColor: buttonColor3,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.zero,
-                            side: BorderSide(color: Colors.white,width: 1),
+                            side: BorderSide(color: Colors.white, width: 1),
                           ),
                         ),
                         child: Text('코드생성'),
@@ -172,7 +192,74 @@ class _apicodeState extends State<apicode> {
                   ],
                 ),
               ),
-              SizedBox(width: 20),//컨테이너 1,2사이 간격
+              SizedBox(width: 20),
+              Container(
+                width: 200,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.white, width: 2),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: ListView(
+                        children: [
+                          ExpansionTile(
+                            leading: Icon(Icons.arrow_right, color: Colors.white), // 화살표 아이콘 추가
+                            title: Text(
+                              '프로젝트1',
+                              style: TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                            children: [
+                              ListTile(
+                                title: Text(
+                                  '룰루1',
+                                  style: TextStyle(color: Colors.white, fontSize: 12),
+                                ),
+                                onTap: () {},
+                              ),
+                              ListTile(
+                                title: Text(
+                                  '룰루2',
+                                  style: TextStyle(color: Colors.white, fontSize: 12),
+                                ),
+                                onTap: () {},
+                              ),
+                            ],
+                          ),
+                          ExpansionTile(
+                            leading: Icon(Icons.arrow_right, color: Colors.white), // 화살표 아이콘 추가
+                            title: Text(
+                              '프로젝트2',
+                              style: TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                            children: [
+                              ListTile(
+                                title: Text(
+                                  '룰루1',
+                                  style: TextStyle(color: Colors.white, fontSize: 12),
+                                ),
+                                onTap: () {},
+                              ),
+                              ListTile(
+                                title: Text(
+                                  '룰루2',
+                                  style: TextStyle(color: Colors.white, fontSize: 12),
+                                ),
+                                onTap: () {},
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: 20),
               Expanded(
                 child: Container(
                   height: 500,
@@ -184,7 +271,177 @@ class _apicodeState extends State<apicode> {
                       width: 2,
                     ),
                   ),
-
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 50),
+                      Text(
+                        '1. 코드 생성을 위한 프레임 워크를 선택해주세요.',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      Row(
+                        children: [
+                          SizedBox(width: 400),
+                          Theme(
+                            data: ThemeData(
+                              unselectedWidgetColor: Colors.white,
+                            ),
+                            child: Checkbox(
+                              value: checkBox1,
+                              onChanged: (bool? value) {
+                                _handleCheckboxChanged(value, 1, 1);
+                              },
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              checkColor: Colors.white,
+                              fillColor: MaterialStateColor.resolveWith(
+                                    (states) {
+                                  if (states.contains(MaterialState.selected)) {
+                                    return Colors.grey;
+                                  }
+                                  return Colors.transparent;
+                                },
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          Text(
+                            'Java Spring',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          Theme(
+                            data: ThemeData(
+                              unselectedWidgetColor: Colors.white,
+                            ),
+                            child: Checkbox(
+                              value: checkBox2,
+                              onChanged: (bool? value) {
+                                _handleCheckboxChanged(value, 1, 2);
+                              },
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              checkColor: Colors.white,
+                              fillColor: MaterialStateColor.resolveWith(
+                                    (states) {
+                                  if (states.contains(MaterialState.selected)) {
+                                    return Colors.grey;
+                                  }
+                                  return Colors.transparent;
+                                },
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'JS Express',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 80),
+                      Text(
+                        '2. 코드 제공받을 방식을 선택해주세요.',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      Row(
+                        children: [
+                          SizedBox(width: 350),
+                          Theme(
+                            data: ThemeData(
+                              unselectedWidgetColor: Colors.white,
+                            ),
+                            child: Checkbox(
+                              value: checkBox3,
+                              onChanged: (bool? value) {
+                                _handleCheckboxChanged(value, 2, 3);
+                              },
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              checkColor: Colors.white,
+                              fillColor: MaterialStateColor.resolveWith(
+                                    (states) {
+                                  if (states.contains(MaterialState.selected)) {
+                                    return Colors.grey;
+                                  }
+                                  return Colors.transparent;
+                                },
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'zip으로 다운받기',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          Theme(
+                            data: ThemeData(
+                              unselectedWidgetColor: Colors.white,
+                            ),
+                            child: Checkbox(
+                              value: checkBox4,
+                              onChanged: (bool? value) {
+                                _handleCheckboxChanged(value, 2, 4);
+                              },
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              checkColor: Colors.white,
+                              fillColor: MaterialStateColor.resolveWith(
+                                    (states) {
+                                  if (states.contains(MaterialState.selected)) {
+                                    return Colors.grey;
+                                  }
+                                  return Colors.transparent;
+                                },
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'github repositery 생성',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 60),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Code generation logic here
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                          child: Text(
+                            "코드생성하기",
+                            style: TextStyle(fontSize: 18, color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
@@ -194,8 +451,7 @@ class _apicodeState extends State<apicode> {
     );
   }
 
-
-  // API 버튼 처리
+  // 페이지 이동 함수들을 apicode 클래스 내에 정의
   void _handleAPIButton(BuildContext context) {
     Navigator.push(
       context,
@@ -210,7 +466,6 @@ class _apicodeState extends State<apicode> {
     );
   }
 
-  // Deployment 버튼 처리
   void _handleDeploymentButton(BuildContext context) {
     Navigator.push(
       context,
@@ -218,7 +473,6 @@ class _apicodeState extends State<apicode> {
     );
   }
 
-  // 로그인 페이지 버튼 처리
   void _handleLoginPageButton(BuildContext context) {
     Navigator.push(
       context,
@@ -226,23 +480,21 @@ class _apicodeState extends State<apicode> {
     );
   }
 
-  // 이미지 버튼 처리
   void _handleImagePressed(BuildContext context) {
     Navigator.popUntil(context, ModalRoute.withName('/'));
   }
-}
 
+  void _handleapipageButton(context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ApiPage()),
+    );
+  }
 
-void  _handleapipageButton(context) {
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => ApiPage()), // sangtae 페이지로 이동
-  );
-}
-
-void _handleapicodeButton(BuildContext context) { // _handleapicodeButton 메서드 추가
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => apicode()), // 적절한 페이지로 이동
-  );
+  void _handleapicodeButton(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => apicode()),
+    );
+  }
 }
